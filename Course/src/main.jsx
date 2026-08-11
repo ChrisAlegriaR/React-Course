da3dc6a159cc99518659c2633cfd89e3f8fb66db
Chris Alegria <christian.alegriar@gmail.com>
2026-08-11 10:05:27 -0600

    Finished: Section 09

--- Course/src/main.jsx
+++ Course/src/main.jsx
@@ -3,8 +3,8 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+import App from './notes/09-Hooks'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
