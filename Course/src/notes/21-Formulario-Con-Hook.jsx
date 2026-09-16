855eedff06ee25d253964126d18eb38be57f47a6
Chris Alegria <christian.alegriar@gmail.com>
2026-09-16 09:52:26 -0600

    Updated: Initial Aspects At Section 21

---
+++ Course/src/notes/21-Formulario-Con-Hook.jsx
@@ -0,0 +1,12 @@
+import ComponenteFormulario from '../components/21-Formulario-Con-Hook'
+import '../styles/21-Formulario-Con-Hook.css'
+
+export default function ComponentePrincipal() {
+    return (
+        <>
+            <h1>Formulario</h1>
+            <hr />
+            <ComponenteFormulario />
+        </>
+    )
+}
