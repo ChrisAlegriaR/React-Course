424658bc81c1c88b676b08c5538fe028e587be74
Chris Alegria <christian.alegriar@gmail.com>
2026-09-18 10:37:16 -0600

    Updated: Events In Form AT Section 21

--- Course/src/components/21-Formulario-Con-Hook.jsx
+++ Course/src/components/21-Formulario-Con-Hook.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react"
+
 export default function ComponenteFormulario() {
+    const [formState, setFormState] = useState({
+        userName: '',
+        email: '',
+        password: ''
+    });
+
+    const { userName, email, password} = useState; //*Agregar que se pueden declarar en un usestate diferntes valores y desestructurarlo para mediante el usop del state del objeto actualizar mediane su set cualquiervalor
+
+    const inputChange = (event) => { //*Agregar que se puede leer desde el event quien ejecuta la funcion en el evento
+        console.log(event.target.name);
+    }
+
     return (
-        
+        <form>
+            <div className="form-group">
+                <label htmlFor="userName">User Name</label>
+                <input type="text" className="form-control" name="userName" placeholder="Enter tour user name" onChange={inputChange}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email address</label>
+                <input type="email" className="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email" onChange={inputChange}/>
+                <small name="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" name="password" placeholder="Password" onChange={inputChange}/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     )
 }
--- Course/src/main.jsx
+++ Course/src/main.jsx
@@ -3,7 +3,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ComponentePrincipal from './notes/21-Formulario-Con-Hook';
-// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
